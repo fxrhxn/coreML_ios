775060480e001c27112731dd4c8e26905d48ce1c
Farhan Rahman <farhanrahman@Fuads-MacBook-Air.local>
2017-11-22 16:09:51 -0500

    Initial Commit

---
+++ coreML_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coreML_ios
+//
+//  Created by Farhan Rahman on 11/22/17.
+//  Copyright © 2017 Farhan Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
