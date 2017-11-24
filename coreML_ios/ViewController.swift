22c92a4181d433f4918e7a7045cb30c3cb518cd8
Farhan Rahman <farhanrahman@Fuads-MacBook-Air.local>
2017-11-24 18:19:38 -0500

    Done

--- coreML_ios/ViewController.swift
+++ coreML_ios/ViewController.swift
@@ -7,12 +7,50 @@
 //
 
 import UIKit
+import AVKit
+import Vision
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Here is where we start the camera.
+        let captureSession = AVCaptureSession()
+        
+        
+        //Get the capture device.
+        guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        
+        //Get the video capture input.
+        let input = try? AVCaptureDeviceInput(device: captureDevice)
+        
+        //Set up where the vid gets input from.
+        captureSession.addInput(input!)
+        
+        //Start the capture session.
+        captureSession.startRunning()
+        
+        //Create a preview layer.
+        var previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        //Add a layer to the view controller.
+        view.layer.addSublayer(previewLayer)
+        
+        //Make the camera go full screen.
+        previewLayer.frame = view.frame
+        
+        //Data output for the video.
+        var dataOutput = AVCaptureVideoDataOutput()
+        
+        //Set a buffer delegate.
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "hahahahahah"))
+        
+        //Add the data output to the capture session.
+        captureSession.addOutput(dataOutput)
+
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +58,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+
+        // Turn the video data into a pixel buffer.
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        
+        guard let model = try? VNCoreMLModel(for: Resnet50().model) else{ return }
+        let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
+            
+            
+            
+            
+            //Check the error, and print out the finished results.
+            guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
+            
+            guard let firstObservation = results.first else { return }
+            
+            print(firstObservation.identifier)
+            print(firstObservation.confidence)
+            
+        }
+        
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+        
+    }
 
 }
 
